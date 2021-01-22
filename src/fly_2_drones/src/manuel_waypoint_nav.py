27e53fec67405b51816a7cb93e1d670f3afd7c1e
chad_samuelson <crsamuelson87@gmail.com>
2021-01-22 12:29:44 -0700

    Fixed 2 drone side-by-side flight

--- src/fly_2_drones/src/manuel_waypoint_nav.py
+++ src/fly_2_drones/src/manuel_waypoint_nav.py
@@ -13,7 +13,6 @@ while True:
     #in_coords = list(map(int,input("\nEnter destination waypoint in NED cooridnates followed by 1/2 for droneID: ").strip().split()))[:4]
     in_coords = list(map(int,input("\nEnter destination waypoint in NED cooridnates: ").strip().split()))[:3]
     print("Flying to ({},{},{}) at 5 m/s".format(in_coords[0],in_coords[1],in_coords[2]))
-    # client.moveToPositionAsync(in_coords[0], in_coords[1], in_coords[2], 5) # This will set the multirotor on a path and imediately return
     client.moveToPositionAsync(in_coords[0], in_coords[1], in_coords[2], 5, vehicle_name="drone_1").join()
     client.moveToPositionAsync(in_coords[0], in_coords[1]+2, in_coords[2], 5, vehicle_name="drone_2").join()
     '''if in_coords[3] == 1:
